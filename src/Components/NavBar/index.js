78de6a9cb6adc556f61a10dbb4267a0bca97f5e8
jainnishant1 <njnishantjain99@gmail.com>
2021-06-18 16:38:22 +0530

    Added Section Navigator

---
+++ src/Components/NavBar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <>
+      {/* <Nav>
+          <NavbarContainer>
+              
+          </NavbarContainer>
+      </Nav> */}
+      <div>Hi</div>
+    </>
+  );
+};
+
+export default NavBar;
